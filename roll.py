a45ccab67f1c63eb955f4e3143d82eab2cbf1ba2
Cole Smith <smithcole@protonmail.com>
2021-04-27 11:24:31 -0700

    made class

---
+++ roll.py
@@ -0,0 +1,35 @@
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+
+
+class Roll:
+    def __init__(self,pins,name,stepsPerTen):
+        self.name = name
+        self.pins= pins
+        self.stepCount= stepsPerTen
+        for pin in pins:
+            GPIO.setup(pin,GPIO.OUT)
+            GPIO.output(pin,0)
+
+
+    def feed(self, count=1):
+
+        seq = [ [1,0,0,0],
+                [1,1,0,0],
+                [0,1,0,0],
+                [0,1,1,0],
+                [0,0,1,0],
+                [0,0,1,1],
+                [0,0,0,1],
+                [1,0,0,1] ]
+        stpes= stepCount*count
+        for i in range(-int(steps)):
+            for halfstep in range(8):
+                for pin in range(4):
+                    GPIO.output(pins[pin], seq[halfstep][pin])
+                time.sleep(0.001)
+ 
