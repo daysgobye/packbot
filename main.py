c0c573355ce5700616e20d13615b01afa12454bd
Cole Smith <smithcole@protonmail.com>
2021-04-28 01:26:54 -0700

    typeo

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ axisz = [15,18,23,24]
 
 diode=Roll(axisx,"Diode",100)
 
-diodeButton1= tk.Button(root,text="diode x1" command=diode.feed)
-diodeButton2= tk.Button(root,text="diode x2" command= lambda: diode.feed(2))
-diodeButton5= tk.Button(root,text="diode x5" command= lambda: diode.feed(5))
-diodeButton6= tk.Button(root,text="diode x6" command= lambda: diode.feed(6))
+diodeButton1= Button(root,text="diode x1" ,command=diode.feed)
+diodeButton2= Button(root,text="diode x2", command= lambda: diode.feed(2))
+diodeButton5= Button(root,text="diode x5", command= lambda: diode.feed(5))
+diodeButton6= Button(root,text="diode x6", command= lambda: diode.feed(6))
 diodeButton1.pack()
 diodeButton2.pack()
 diodeButton5.pack()
