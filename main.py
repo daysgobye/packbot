3983352b3648a9f9fb7b4c877807aa3e4a18edf1
Cole Smith <smithcole@protonmail.com>
2021-04-28 01:20:51 -0700

    removed class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from roll import Roll
-import tkinter as tk
+Tkinter  import *
 
 axisx = [4,17,27,22]
 axisy = [10,9,11,7]
@@ -10,33 +10,18 @@ axisz = [15,18,23,24]
 
 diode=Roll(axisx,"Diode",100)
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.diodeButton1= tk.Button(self,text="diode x1" command=diode.feed)
-        self.diodeButton2= tk.Button(self,text="diode x2" command= lambda: diode.feed(2))
-        self.diodeButton5= tk.Button(self,text="diode x5" command= lambda: diode.feed(5))
-        self.diodeButton6= tk.Button(self,text="diode x6" command= lambda: diode.feed(6))
-        self.diodeButton1.pack()
-        self.diodeButton2.pack()
-        self.diodeButton5.pack()
-        self.diodeButton6.pack()
-
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
+diodeButton1= tk.Button(root,text="diode x1" command=diode.feed)
+diodeButton2= tk.Button(root,text="diode x2" command= lambda: diode.feed(2))
+diodeButton5= tk.Button(root,text="diode x5" command= lambda: diode.feed(5))
+diodeButton6= tk.Button(root,text="diode x6" command= lambda: diode.feed(6))
+diodeButton1.pack()
+diodeButton2.pack()
+diodeButton5.pack()
+diodeButton6.pack()
     
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+root = Tk()
+Tk.mainloop()
 
 except KeyboardInterrupt:
   print "  Quit"
